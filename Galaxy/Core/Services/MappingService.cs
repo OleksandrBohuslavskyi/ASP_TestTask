917bc92fb1f403606303c0ae79abf02f0b0f2da8
Olexandr Bohuslavskyi <olexandr.bohuslavskyi@gmail.com>
2016-08-08 22:52:47 +0300

    [+] Added mapper service

--- Galaxy/Core/Services/MappingService.cs
+++ Galaxy/Core/Services/MappingService.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Core.DTOModels;
+using Data.Entities;
 
 namespace Core.Services
 {
     class MappingService
     {
-        
     }
 }
